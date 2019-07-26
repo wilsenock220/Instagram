b1da8075abdf20cc84bd71bcacff48190d835a26
wilsenock220 <wilsenock220@gmail.com>
2019-07-26 13:31:53 +0300

    add login and logout urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    
     url('index/', views.index, name='index'),
     url('register/', views.register, name='register'),
+    url('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
 
 if settings.DEBUG:
